a05802510dabfc4ed7dc330ec043d65588c28540
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-03-25 22:18:35 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+# TODO set to boot to command-line
+# TODO enable SSH
+# TODO install vim
+# TODO install rust
+# TODO download and unzip latest system-stats-dashboard release
+# TODO download and unzip latest blog-server release
+# TODO download .service files and copy them to /etc/systemd/system
+# TODO enable services
+# TODO reboot
