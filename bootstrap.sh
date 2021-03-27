a6cabd7e848316ef6cdd076ad00bdc60dd1df39c
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-03-27 18:17:57 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,11 +20,13 @@ curl https://sh.rustup.rs -sSf | sh
 # install latest system-stats-dashboard release
 wget -O system-stats-dashboard.zip https://github.com/rotoclone/system-stats-dashboard/releases/latest/download/raspberrypi.zip
 unzip system-stats-dashboard.zip -d /home/pi/system-stats-dashboard
+chmod +x /home/pi/system-stats-dashboard/system-stats-dashboard
 rm -f system-stats-dashboard.zip
 
 # install latest blog-server release
 wget -O blog-server.zip https://github.com/rotoclone/blog-server/releases/latest/download/raspberrypi.zip
 unzip blog-server.zip -d /home/pi/blog-server
+chmod +x /home/pi/blog-server/blog-server
 rm -f blog-server.zip
 
 # TODO download .service files and copy them to /etc/systemd/system
