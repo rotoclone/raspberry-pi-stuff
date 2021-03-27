161234e7b3923e3e099821bee1a6f66c6d7adedd
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-03-27 18:02:40 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,35 @@
+# exit on error
+set -e
+
 # TODO set to boot to command-line
 # TODO enable SSH
-# TODO install vim
-# TODO install rust
-# TODO install nginx
-# TODO install certbot (with nginx extension)
+
+# install vim
+apt-get install vim
+
+# install nginx
+apt-get install nginx
+
+# install certbot (with nginx extension)
+apt-get install certbot
+apt-get install python-certbot-nginx
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh
+
 # TODO download and unzip latest system-stats-dashboard release
 # TODO download and unzip latest blog-server release
 # TODO download .service files and copy them to /etc/systemd/system
 # TODO download nginx config files and copy them to the correct locations
-# TODO enable services
-# TODO run certbot so it sets up the auto-renew stuff
-# TODO reboot
+
+# enable services
+systemctl daemon-reload
+systemctl enable nginx
+systemctl enable systemstatsdashboard
+systemctl enable blogserver
+
+# run certbot so it sets up the auto-renew stuff
+certbot --nginx -d rotoclone.zone -d www.rotoclone.zone
+
+echo "Success! Rebooting..."
+reboot
