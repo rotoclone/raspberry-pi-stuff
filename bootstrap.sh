60fae76ded89cb0b79b5696e9a430a0514108494
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-03-27 17:26:16 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # TODO install vim
 # TODO install rust
 # TODO install nginx
-# TODO install certbot
+# TODO install certbot (with nginx extension)
 # TODO download and unzip latest system-stats-dashboard release
 # TODO download and unzip latest blog-server release
 # TODO download .service files and copy them to /etc/systemd/system
