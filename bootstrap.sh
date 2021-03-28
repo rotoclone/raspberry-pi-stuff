733bb5edb6b3da44b1b501b19bd4492c2a4b47ac
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-03-27 20:26:49 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,66 +3,82 @@ set -e
 
 # TODO set to boot to command-line
 
-# install vim
+echo "Installing vim..."
 apt-get install vim
+echo "Done"
 
-# install nginx
+echo "Installing nginx..."
 apt-get install nginx
+echo "Done"
 
-# install certbot (with nginx extension)
+echo "Installing certbot..."
 apt-get install certbot
 apt-get install python-certbot-nginx
+echo "Done"
 
-# install rust
+echo "Installing rust..."
 curl https://sh.rustup.rs -sSf | sh
+echo "Done"
 
-# install docker
+echo "Installing docker..."
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
+echo "Done"
 
-# install pip
+echo "Installing pip..."
 apt install python3-pip
+echo "Done"
 
-# install docker-compose
+echo "Installing docker-compose..."
 pip3 -v install docker-compose
+echo "Done"
 
-# install shynet
+echo "Installing shynet..."
 #TODO
+echo "Done"
 
-# install latest system-stats-dashboard release
+echo "Installing system-stats-dashboard..."
 wget -O system-stats-dashboard.zip https://github.com/rotoclone/system-stats-dashboard/releases/latest/download/raspberrypi.zip
 unzip system-stats-dashboard.zip -d /home/pi/system-stats-dashboard
 chmod +x /home/pi/system-stats-dashboard/system-stats-dashboard
 rm -f system-stats-dashboard.zip
+echo "Done"
 
-# install latest blog-server release
+echo "Installing blog-server..."
 wget -O blog-server.zip https://github.com/rotoclone/blog-server/releases/latest/download/raspberrypi.zip
 unzip blog-server.zip -d /home/pi/blog-server
 chmod +x /home/pi/blog-server/blog-server
 rm -f blog-server.zip
+echo "Done"
 
-# set up system-stats-dashboard systemd service
+echo "Setting up system-stats-dashboard systemd service..."
 wget -O systemstatsdashboard.service https://github.com/rotoclone/raspberry-pi-stuff/raw/master/systemd/systemstatsdashboard.service
 mv systemstatsdashboard.service /etc/systemd/system/
+echo "Done"
 
-# set up blog-server systemd service
+echo "Setting up blog-server systemd service..."
 wget -O blogserver.service https://github.com/rotoclone/raspberry-pi-stuff/raw/master/systemd/blogserver.service
 mv blogserver.service /etc/systemd/system/
+echo "Done"
 
-# set up nginx
+echo "Setting up nginx config..."
 wget -O rotoclone.zone.conf https://github.com/rotoclone/raspberry-pi-stuff/raw/master/nginx/rotoclone.zone.conf
 mv rotoclone.zone.conf /etc/nginx/conf.d/
+echo "Done"
 
 # run certbot so it sets up the auto-renew stuff
+echo "Running certbot..."
 certbot --nginx -d rotoclone.zone -d www.rotoclone.zone
+echo "Done"
 
-# enable services
+echo "Enabling services..."
 systemctl daemon-reload
 systemctl enable ssh
 systemctl enable nginx
 systemctl enable docker
 systemctl enable systemstatsdashboard
 systemctl enable blogserver
+echo "Done"
 
 echo "Success! Rebooting..."
 reboot
