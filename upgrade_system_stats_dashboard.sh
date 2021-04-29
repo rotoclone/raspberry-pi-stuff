2fe4fd402264c6446130d17bf1c75713300bb15f
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-04-29 01:11:39 -0500

    Update upgrade_system_stats_dashboard.sh

--- upgrade_system_stats_dashboard.sh
+++ upgrade_system_stats_dashboard.sh
@@ -2,11 +2,15 @@ SERVICE_NAME=systemstatsdashboard
 BINARY_NAME=system-stats-dashboard
 GIT_DIR=/home/pi/git/${BINARY_NAME}
 TARGET_DIR=/home/pi/${BINARY_NAME}
+TEMP_DIR=/home/pi/temp_${SERVICE_NAME}
 
 systemctl stop $SERVICE_NAME
+cp -r ${TARGET_DIR}/stats_history ${TEMP_DIR}
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
 cp ${GIT_DIR}/target/release/${BINARY_NAME} ${TARGET_DIR}/
 cp ${GIT_DIR}/Rocket.toml ${TARGET_DIR}/
 cp -r ${GIT_DIR}/templates ${TARGET_DIR}/
+cp -r ${TEMP_DIR}/stats_history ${TARGET_DIR}
+rm -rf $TEMP_DIR
 systemctl start $SERVICE_NAME
