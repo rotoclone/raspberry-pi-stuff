3bd09dd08cab88f067b87d8ba2fb182e7a4d05f2
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-04-29 01:31:15 -0500

    Update upgrade_system_stats_dashboard.sh

--- upgrade_system_stats_dashboard.sh
+++ upgrade_system_stats_dashboard.sh
@@ -10,12 +10,12 @@ systemctl stop $SERVICE_NAME
 cp -r ${TARGET_DIR}/stats_history ${TEMP_DIR}/
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
-cp ${GIT_DIR}/target/release/${BINARY_NAME} ${TARGET_DIR}/
-cp ${GIT_DIR}/Rocket.toml ${TARGET_DIR}/
-cp -r ${GIT_DIR}/templates ${TARGET_DIR}/
 mkdir ${TARGET_DIR}/stats_history
 chmod a+w ${TARGET_DIR}/stats_history
 cp -r ${TEMP_DIR}/stats_history ${TARGET_DIR}/
+cp ${GIT_DIR}/target/release/${BINARY_NAME} ${TARGET_DIR}/
+cp ${GIT_DIR}/Rocket.toml ${TARGET_DIR}/
+cp -r ${GIT_DIR}/templates ${TARGET_DIR}/
 systemctl start $SERVICE_NAME
 
 rm -rf $TEMP_DIR
