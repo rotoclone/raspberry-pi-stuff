535765950c8e50d5343fd4e1526f7864e8fd9377
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-04-29 01:30:05 -0500

    Update upgrade_system_stats_dashboard.sh

--- upgrade_system_stats_dashboard.sh
+++ upgrade_system_stats_dashboard.sh
@@ -4,6 +4,8 @@ GIT_DIR=/home/pi/git/${BINARY_NAME}
 TARGET_DIR=/home/pi/${BINARY_NAME}
 TEMP_DIR=/home/pi/temp_${SERVICE_NAME}
 
+mkdir $TEMP_DIR
+
 systemctl stop $SERVICE_NAME
 cp -r ${TARGET_DIR}/stats_history ${TEMP_DIR}/
 rm -rf $TARGET_DIR
@@ -14,5 +16,6 @@ cp -r ${GIT_DIR}/templates ${TARGET_DIR}/
 mkdir ${TARGET_DIR}/stats_history
 chmod a+w ${TARGET_DIR}/stats_history
 cp -r ${TEMP_DIR}/stats_history ${TARGET_DIR}/
-rm -rf $TEMP_DIR
 systemctl start $SERVICE_NAME
+
+rm -rf $TEMP_DIR
